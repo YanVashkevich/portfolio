25ea35a1a437054b260d68f5b64ac9523534fb63
Yan <yanvashkevich@gmail.com>
2023-07-16 17:30:21 +0300

    changed font size in works section

--- portfolio/src/components/Works/Works.jsx
+++ portfolio/src/components/Works/Works.jsx
@@ -6,16 +6,18 @@ export const Works = () => {
 
   return (
     <div className='works'>
-      <div className='whole-page'>
-        <ul className='works-left'>
-          <div className='wd'>Web Design</div>
-          <div className='dev'>Development</div>
-          <div className='ilu'>Ilustration</div>
-          <div className='wp'>Web-pages</div>
-          <div className='sm'>Social media</div>
-        </ul>
-        <div className='works-right'></div>
-      </div>
+        <div className='left'>
+          <ul className='works-left'>
+            <div className='wd'>Web Design</div>
+            <div className='dev'>Development</div>
+            <div className='ilu'>Ilustration</div>
+            <div className='wp'>Web-pages</div>
+            <div className='sm'>Social media</div>
+          </ul>
+        </div>
+        <div className='works-right'>
+          
+        </div>
     </div>
   )
 }
